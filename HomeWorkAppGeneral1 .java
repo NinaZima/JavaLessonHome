3d25df90d48919a291d505f81a6921f53abde695
NinaZima <90524302+NinaZima@users.noreply.github.com>
2021-09-20 19:14:02 +0300

    Add files via upload

---
+++ HomeWorkAppGeneral1 .java
@@ -0,0 +1,40 @@
+class HomeWorkAppGeneral1 {
+    public static void main(String[]args){
+		checkSumSing(8, 4);
+		printColor();
+		printThreeWords();
+		compareNumbers();
+	}
+	
+	static void printThreeWords(){
+	        System.out.println("Orange");
+		    System.out.println("Banana");
+		    System.out.println("Apple");
+	}
+	
+	static void checkSumSing(int a, int b){
+	    int c = a + b;
+	        System.out.println(c >= 0?  "Positive amount" :"The amount is negative");
+	}
+	
+	static void printColor(){
+	    int value = 95;
+	    if (value <= 0) {
+		    System.out.println("Red"); 
+	    } else if (value > 0 && value <= 100) {
+		    System.out.println("Yellow"); 
+	    } else {
+		    System.out.println("Green"); 
+	}
+		
+    static void compareNumbers() {
+	    int a = 11;
+	    int b = 23;
+	    if (a >= b ){
+		    System.out.println("a >= b");
+	    } else  {
+	        System.out.println("a < b");		
+	}
+	}
+	}
+}
