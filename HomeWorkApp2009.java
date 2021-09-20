b9328e6a7dd00f5cd08def1ec378efd0c46f33f9
NinaZima <90524302+NinaZima@users.noreply.github.com>
2021-09-20 13:34:32 +0300

    Add files via upload

---
+++ HomeWorkApp2009.java
@@ -0,0 +1,17 @@
+public class HomeWorkApp2009 {
+	public static void main(String[] args) {
+		howIsTheWeatherThere();
+	}
+	public static void howIsTheWeatherThere() {
+	int temperature = 15;
+    if(temperature > 10 ) {
+		System.out.println("It's very warm outside for the month of March"); //На улице очень даже тепло для марта месяца
+	}	else if (temperature > -5){
+		System.out.println("For March, the very thing"); //Для марта самое то
+		
+	}	else {
+	        	System.out.println("Is it spring there?"); //Там точно весна?
+		
+	}
+	}
+}
